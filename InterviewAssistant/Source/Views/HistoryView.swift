4e2efaf1df3ed8d107777753181a8c31c5a07243
ambitiousjulian <juliancajuste@gmail.com>
2025-01-12 11:38:03 -0600

    Clean repository and update gitignore

--- InterviewAssistant/Source/Views/HistoryView.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-struct HistoryView: View {
-    var body: some View {
-        Text("History Coming Soon")
-    }
-}
--- InterviewAssistant/Source/Views/MockInterviewView.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-struct MockInterviewView: View {
-    var body: some View {
-        Text("Mock Interview Coming Soon")
-    }
-}
