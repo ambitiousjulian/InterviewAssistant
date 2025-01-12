06b6934b532b3a64fea48c2b6ce520972d0ddcd7
ambitiousjulian <juliancajuste@gmail.com>
2025-01-12 09:39:03 -0600

    Initial Commit

---
+++ InterviewAssistant/InterviewAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterviewAssistantApp.swift
+//  InterviewAssistant
+//
+//  Created by Julian Cajuste on 1/12/25.
+//
+
+import SwiftUI
+
+@main
+struct InterviewAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
